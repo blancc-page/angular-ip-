9f45a71917487718dc739fb83d9a0dcbde017afa
Moses Muta <65918325+blancc-page@users.noreply.github.com>
2022-04-13 09:07:10 +0300

    branch away from main

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-ip-'),
+      dir: require('path').join(__dirname, './coverage/week1-angular-ip'),
       subdir: '.',
       reporters: [
         { type: 'html' },
